c8f136622f7e6a6879e3009780ad42588e22df7d
Wafaamin61 <Wafaamiminyaoui61@gmail.com>
2023-05-26 14:41:34 -0700

    allo

--- main.h
+++ main.h
@@ -19,4 +19,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * struct fmt - Struct op
+ *
+ * @fmt: The format.
+ * @fn: The function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
 
